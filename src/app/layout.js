9b46e00ec314468cc64b8c51d73f8e858c84e4b0
AtTheRate-anuragjyoti <anuragjyoti.dev@gmail.com>
2024-11-25 12:00:16 +0530

    HTML tag for SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" dir="ltr">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <head>
         <meta name="google-site-verification" content="Dibu5YjA3RGtBImMW0z-No4vAdfHxHdRNS3EsHAnuLM" />
+        </head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:type" content="website" />
