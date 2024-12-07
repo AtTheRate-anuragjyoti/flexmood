c7433073454991711f0a5efd1e22528b65f219f2
AtTheRate-anuragjyoti <anuragjyoti.dev@gmail.com>
2024-12-07 09:13:47 +0530

    added GA

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Flexmood | Master Productivity with Premium Ebooks & Smart Resources",
@@ -30,6 +31,20 @@ export default function RootLayout({ children }) {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8FBVT2MFT3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-8FBVT2MFT3');
+          `}
+        </Script>
       </head>
       <body>
             {children}
