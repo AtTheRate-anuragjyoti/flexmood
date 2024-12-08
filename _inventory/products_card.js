a56bec89e5996e6dcc0a77500f363f6f53c3d626
AtTheRate-anuragjyoti <anuragjyoti.dev@gmail.com>
2024-12-08 10:31:44 +0530

    added shine

--- _inventory/products_card.js
+++ _inventory/products_card.js
@@ -1,18 +1,14 @@
-// components/ui/products_card.tsx
 'use client';
 
 import Link from 'next/link';
 import Image from 'next/image';
 import userRegion from '@/utils/region';
 import localFont from 'next/font/local';
-import { ShoppingCart, Star } from 'lucide-react';
-import styles from './styles.module.css';
+import { ShoppingCart, Star } from 'lucide-react'; // Using both cart and star icons
 
 const spaceGrotesk = localFont({ src: './spaceGrotesk.ttf' });
 
-
-
-const ProductCard = ({ coverImage, title, price, serial }) => {
+const ProductCard = ({ coverImage, title, price, purchaseLink, serial }) => {
   const region = userRegion((state) => state.region);
 
   const truncateTitle = (text, maxLength) => {
@@ -22,13 +18,13 @@ const ProductCard = ({ coverImage, title, price, serial }) => {
   const renderPrice = () => {
     if (price === 0) {
       return (
-        <div className="text-md font-bold text-gray-700 mb-4 relative z-10">
+        <div className="text-md font-bold text-gray-700 mb-4">
           Free
         </div>
       );
     }
     return (
-      <div className="text-md font-semibold text-gray-700 mb-4 relative z-10">
+      <div className="text-md font-semibold text-gray-700 mb-4">
         {region === 'India' ? '₹' : '$'}
         {price}
       </div>
@@ -36,27 +32,27 @@ const ProductCard = ({ coverImage, title, price, serial }) => {
   };
 
   return (
-    <div className={`w-full bg-white shadow-lg border border-gray-700 rounded-lg overflow-hidden relative ${price === 0 ? styles.freeProductShine : ''}`}>
-      <div className={`bg-gray-900 flex items-center justify-center relative ${price === 0 ? styles.greyBackground : ''}`}>
+    <div className="w-full bg-white shadow-lg border border-gray-700 rounded-lg overflow-hidden">
+      <div className="bg-gray-900 flex items-center justify-center">
         <Image
           src={coverImage}
           alt={`Cover of ${title} by Flexmood`}
           width={150}
           height={200}
-          className="relative z-10"
         />
       </div>
-      <div className="p-4 relative z-10">
+      <div className="p-4">
         <Link href={`/${region === 'India' ? 'india' : 'international'}/${serial}`} passHref>
-          <h2 className="text-lg font-bold text-gray-800 mb-2 hover:text-[#334247] transition-all relative z-10">
+          <h2 className="text-lg font-bold text-gray-800 mb-2 hover:text-[#334247] transition-all">
             {truncateTitle(title, 30)}
           </h2>
         </Link>
         {renderPrice()}
-        <Link 
+        <Link
           href={region === 'India' ? `/purchase/india/${serial}` : `/purchase/international/${serial}`}
         >
-          <button className="w-full flex items-center justify-center gap-2 rounded-md bg-gray-800 py-3 text-white transition-transform duration-300 hover:bg-gray-700 relative z-10">
+          <button className="w-full flex items-center justify-center gap-2 rounded-md bg-gray-800 py-3 text-white transition-transform duration-300 hover:bg-gray-700">
+            {/* Conditionally render the icon based on price */}
             {price === 0 ? (
               <Star className="w-5 h-5" />
             ) : (
